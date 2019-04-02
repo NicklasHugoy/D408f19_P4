1b89b7579fc642da43b1ee44ad6f8f8871add9a7
Unknown <thorulf@live.dk>
2019-04-02 09:44:42 +0200

    Added missing elements to a Node

--- src/main/java/dk/aau/cs/AST/Nodes/WhileLoop.java
+++ src/main/java/dk/aau/cs/AST/Nodes/WhileLoop.java
@@ -1,9 +1,21 @@
 package dk.aau.cs.AST.Nodes;
 
 import dk.aau.cs.AST.ASTVisitor;
+import dk.aau.cs.AST.Expression;
 import dk.aau.cs.AST.Statement;
 
+import java.util.List;
+
 public class WhileLoop implements Statement {
+
+    public Expression expression;
+    public List<Statement> statements;
+
+    public WhileLoop(Expression expression, List<Statement> statements) {
+        this.expression = expression;
+        this.statements = statements;
+    }
+
     @Override
     public <T> T accept(ASTVisitor<T> visitor) {
         return visitor.visitWhileLoop(this);
