eef6649f709ca7a1eb3fbebc782a19efd4a2512d
Nicklas Hjortshøj Hugøy <Nicklas.hug@gmail.com>
2019-04-05 10:23:54 +0200

    LiteralVector implements equals

--- src/main/java/dk/aau/cs/AST/Nodes/LiteralVector.java
+++ src/main/java/dk/aau/cs/AST/Nodes/LiteralVector.java
@@ -5,6 +5,8 @@ import dk.aau.cs.AST.Expression;
 import dk.aau.cs.AST.Node;
 import dk.aau.cs.AST.Value;
 
+import java.util.Objects;
+
 public class LiteralVector implements Expression, Value {
 
     public Expression x,y,z;
@@ -29,4 +31,17 @@ public class LiteralVector implements Expression, Value {
     public String toString() {
         return "Vector";
     }
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		LiteralVector that = (LiteralVector) o;
+		return Objects.equals(x, that.x) && Objects.equals(y, that.y) && Objects.equals(z, that.z);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y, z);
+	}
 }
