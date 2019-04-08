2081ee4efd65da833f8b291612aa2abf156fa716
Frederik Christensen <frede_rind@hotmail.dk>
2019-04-08 10:26:33 +0200

    Added tree printer test

---
+++ src/test/java/dk/aau/cs/AST/TreePrinterTest.java
@@ -0,0 +1,72 @@
+package dk.aau.cs.AST;
+
+import dk.aau.cs.AST.Nodes.Node;
+import dk.aau.cs.ErrorReporting.WarningLevel;
+import dk.aau.cs.Syntax.GMMLexer;
+import dk.aau.cs.Syntax.GMMParser;
+import org.antlr.v4.runtime.BufferedTokenStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.TokenStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TreePrinterTest {
+
+    ASTGenerator astGenerator;
+    @BeforeEach
+    void setUp() {
+        astGenerator = new ASTGenerator();
+    }
+
+
+    GMMParser getParser(CharStream cs) {
+        GMMLexer lexer = new GMMLexer(cs);
+        TokenStream ts = new BufferedTokenStream(lexer);
+        return new GMMParser(ts);
+    }
+    @Test
+    void treePrinter(){
+        CharStream cs = CharStreams.fromString("block[tool:0, bound_x:200]{ move X=2 Y=5 leftCircle X=5 R=2}");
+        GMMParser parser = getParser(cs);
+        Node node = astGenerator.visitProg(parser.prog());
+        TreePrinter tp = new TreePrinter(node);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        tp.Print();
+
+        String expected = "Program\r\n" +
+                "|  Block definition\r\n" +
+                "|  |  MachineOption\r\n" +
+                "|  |  |  ID = tool\r\n" +
+                "|  |  |  Num = 0.0\r\n" +
+                "|  |  MachineOption\r\n" +
+                "|  |  |  ID = bound_x\r\n" +
+                "|  |  |  Num = 200.0\r\n" +
+                "|  |  Move\r\n" +
+                "|  |  |  Relative Parameter\r\n" +
+                "|  |  |  |  ID = X\r\n" +
+                "|  |  |  |  Num = 2.0\r\n" +
+                "|  |  |  Relative Parameter\r\n" +
+                "|  |  |  |  ID = Y\r\n" +
+                "|  |  |  |  Num = 5.0\r\n" +
+                "|  |  Left Circle\r\n" +
+                "|  |  |  Relative Parameter\r\n" +
+                "|  |  |  |  ID = X\r\n" +
+                "|  |  |  |  Num = 5.0\r\n" +
+                "|  |  |  Relative Parameter\r\n" +
+                "|  |  |  |  ID = R\r\n" +
+                "|  |  |  |  Num = 2.0\r\n";
+
+        assertEquals(expected, outContent.toString());
+    }
+}
